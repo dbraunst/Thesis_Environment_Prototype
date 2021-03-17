e144f92b4eb047737cbd6be283dd8581198a32dd
Daniel Braunstein <dbraunst@umich.edu>
2021-03-17 15:05:36 -0400

    Reorganized; Added DontDestroy.cs
    Added dontdestroy.cs to add to persistent objects
    Added More scenes, organized in build settings
    Saved TestPrinter as prefab
    TODO: ensure TestPrinter persistence is working && StreamWriter stays accessible after scene load

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Mime;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+using Debug = UnityEngine.Debug;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
+        void Pause() 
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f; //freezes global time
+            GameIsPaused = true;
+        }
+
+        public void Resume()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1.0f; //freezes global time
+            GameIsPaused = false;
+        }
+
+    public void LoadMenu() 
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Loading Menu"); 
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame() 
+    {
+        Debug.Log("Quitting Game... ");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public SceneManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
